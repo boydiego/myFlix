87aceb577768a9da120cf38c7348c0c18fa52d7f
Diego <diegolive5000@gmail.com>
2022-05-24 15:57:11 +0200

    remove unecessary files, set the environment var for mongoose.connect

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ const app = express(),
   Movies = Models.Movie,
   Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
